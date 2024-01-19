3a5a48351276eaee0ff8df19a000b557882a4c91
Soumya Srivastava <sri.soumya0607@gmail.com>
2024-01-19 20:34:50 +0530

    added express

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
 require("dotenv").config();
+const express = require("express");
 const { Client, Collection, GatewayIntentBits } = require("discord.js");
 const { regsiterCommand } = require("./register-commands");
+
+const app = express();
+app.set("view engine", "ejs");
+app.get("/", (req, res) => {
+  res.render("index.ejs");
+});
 
 try {
   const client = new Client({
@@ -20,3 +27,7 @@ try {
 
   regsiterCommand(client);
 } catch (error) {}
+
+app.listen(process.env.PORT, () =>
+  console.log(`Server running on port ${process.env.PORT}`)
+);
